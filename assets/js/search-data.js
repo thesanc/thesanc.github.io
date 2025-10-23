c32ca597c6a0532af2231af67226fd5154a5ae32
thesanc <theophile.sanchez@gmail.com>
2025-10-23 15:11:49 +0000

    Deploying to gh-pages from @ thesanc/thesanc.github.io@c11694be50ab88a270df1a7d78031b4bd0def0c8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%65%6D%61%69%6C@%74%68%65%6F%70%68%69%6C%65%73%61%6E%63%68%65%7A.%66%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://orcid.org/0000-0001-8571-0578", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,14 +522,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=JgnWQzgAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
