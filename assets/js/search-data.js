55dbaeecbfc7ffffd401a339e637d0c32ad56eb9
thesanc <theophile.sanchez@gmail.com>
2025-10-24 21:00:33 +0000

    Deploying to gh-pages from @ thesanc/thesanc.github.io@3dad76a38a95373288cd15bce1b6c099351f4b5b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,13 +53,6 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0001-8571-0578", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
